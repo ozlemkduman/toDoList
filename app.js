4eb937a2f397bda8852696b2c3d7327c1369f6aa
ozlemkduman <ozlemk.duman@gmail.com>
2023-03-01 16:30:13 +0300

    for todos

--- app.js
+++ app.js
@@ -12,24 +12,37 @@ function main() {
       createTodoEl(toDo);
     });
   }
-  
-  
 }
 
 main();
+function sendWithEnter(e) {
+  if (e.key == "Enter") {
+    newElement();
+  }
+}
+taskInput.addEventListener("keydown", sendWithEnter);
 
 function newElement() {
+  const todos = JSON.parse(localStorage.getItem("todos"));
+  const todoName = taskInput.value;
   if (taskInput.value.trim() == "") {
     showToastMessage("Cannot add with empty string", true);
   } else {
-    const todoName = taskInput.value;
     const todo = { name: todoName, done: false };
-    const todos = JSON.parse(localStorage.getItem("todos"));
     todos.push(todo);
     localStorage.setItem("todos", JSON.stringify(todos));
     createTodoEl(todo);
     taskInput.value = "";
     showToastMessage("Todo item is added", false);
+  }
+
+  for (let i = 0; i < todos.length; i++) {
+    if(todos[i].name==todoName){
+      console.log("var");
+    }else{
+      console.log("yok");
+    }
+    
   }
 }
 
@@ -88,10 +101,9 @@ function createTodoEl(todo) {
   checkboxEl.type = "checkbox";
   checkboxEl.checked = todo.done;
   const liEl = document.createElement("li");
-  
+
   let todos = JSON.parse(localStorage.getItem("todos"));
 
-  
   checkboxEl.addEventListener("click", checkboxDone);
   // TODO done
 
@@ -101,27 +113,22 @@ function createTodoEl(todo) {
   liEl.append(deleteParent);
   list.append(liEl);
 
-
-  todos.forEach(td=>{
-    console.log(td.done);
-    const liText= liEl.getElementsByTagName("div")[0].textContent
-    if(td.done==true){
+  todos.forEach((td) => {
+    const liText = liEl.getElementsByTagName("div")[0].textContent;
+    if (td.done == true) {
       // o td.name ile aynı texte sahip olan li'nin clasını "checked" yap
-      if(td.name===liText){
-        liEl.className="checked"
+      if (td.name === liText) {
+        liEl.className = "checked";
       }
     }
-  })
+  });
 }
 
 function deleteLocalTodoByName(todoName) {
   let todos = JSON.parse(localStorage.getItem("todos"));
-
-  console.log(todos);
   for (var i = 0; i < todos.length; i++) {
     if (todos[i].name === todoName) {
       todos.splice(i, 1); // changes array in place
-      console.log(todos);
       localStorage.setItem("todos", JSON.stringify(todos));
     }
   }
